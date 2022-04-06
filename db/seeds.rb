9da235efa007e2d1aa557592cde9d5c9c9373d3c
karsyn ansari <karsyn.ansari@gmail.com>
2022-04-06 11:24:39 -0600

    login, stay logged in, and log out working on backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "seeding users"
+U1 = User.create(username: "Peter", password_digest: "")
+U2 = User.create(username: "Karsyn", password_digest: "")
+puts "users seeded"

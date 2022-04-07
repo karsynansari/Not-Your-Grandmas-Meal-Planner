aa5a5ea0aad860b21075c6333535e42cb99e291f
karsyn ansari <karsyn.ansari@gmail.com>
2022-04-07 14:47:51 -0600

    search function working on hp

--- client/src/Components/Homepage.js
+++ client/src/Components/Homepage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
+import Search from "./Search";
 import RecipeCardContainer from "./RecipeCardContainer";
 
 function Homepage({ user, onLogout }) {
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("")
   console.log(recipes);
 
   useEffect(() => {
@@ -11,11 +13,17 @@ function Homepage({ user, onLogout }) {
       .then((response) => response.json())
       .then((data) => setRecipes(data));
   }, []);
+
+  //make this also include cuisine, meal type, etc
+  const searchedRecipes = recipes.filter((recipe)=>
+  recipe.title.toLowerCase().includes(search.toLowerCase())
+  )
   return (
     <>
       <NavBar onLogout={onLogout} user={user} />
       <div>Homepage</div>
-      <RecipeCardContainer recipes={recipes} />
+      <Search search={search} setSearch={setSearch}/>
+      <RecipeCardContainer recipes={searchedRecipes} />
     </>
   );
 }

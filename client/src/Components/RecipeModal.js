0ea4aa2c8637ae5ca976aee181e2b9b6b88e92b2
karsyn ansari <karsyn.ansari@gmail.com>
2022-04-10 15:12:17 -0600

    all modal info rendering

--- client/src/Components/RecipeModal.js
+++ client/src/Components/RecipeModal.js
@@ -3,26 +3,19 @@ import { Modal } from 'react-bootstrap'
 
 function RecipeModal({setRecipeCardClicked, clickedRecipe}) {
   const [show, setShow] = useState(true);
+  const {id, cuisine, title, image, meal_type, serving, prep_time, cook_time} = clickedRecipe
   const recipeDirections = clickedRecipe.directions 
-  // console.log(recipeDirections)
-  // const eachDirection = recipeDirections.map((direction) => (direction.instructions))
-  // console.log(eachDirection)
+  const recipeIngredients = clickedRecipe.ingredients
+  console.log(recipeIngredients)
+  console.log(clickedRecipe)
 
   const handleClose = () => setShow(false);
+
   //this is making the recipe card show state change back to false when you close the modal. 
 function handleUpdateRecipeCardClickedState()  {
 setRecipeCardClicked((value) => !value);
 }
   return (
-    // {thequestions[currentQuestion].quiz_answers.map((answer) => (
-    //         <Button
-    //           variant={"outline-dark"}
-    //           onClick={() => handleAnswerButtonClick(answer)}
-    //           size="lg"
-    //         >
-    //           {answer.answerText}
-    //         </Button>
-    //       ))}
     <>
       <Modal
         show={show}
@@ -33,9 +26,25 @@ setRecipeCardClicked((value) => !value);
       >
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
-          <h3>{clickedRecipe.title}</h3>
+          <img src={image} alt={title}></img>
+          <h3>{title}</h3>
+          <p>Cuisine: {cuisine}</p>
+          <p>Course: {meal_type}</p>
+          <p>Servings: {serving}</p>
+          <p>Prep Time: {prep_time}</p>
+          <p>Cook Time: {cook_time}</p>
+          <p>Ingredients</p>
+          {recipeIngredients.map((ingredient) => (
+            <ul>
+              {ingredient.quantity} {ingredient.measurement}{" "}
+              {ingredient.each_ingredient}
+            </ul>
+          ))}
+          <p>Directions</p>
           {recipeDirections.map((direction) => (
-            <li>{direction.step}. {direction.instructions}</li>
+            <ul key={direction.id}>
+              {direction.step}. {direction.instructions}
+            </ul>
           ))}
         </Modal.Body>
       </Modal>

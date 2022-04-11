50c1b66526bff714717fc11071d62b293763eccf
karsyn ansari <karsyn.ansari@gmail.com>
2022-04-11 12:01:41 -0600

    create action for meal plans working on backend

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -4,4 +4,14 @@ class MealPlansController < ApplicationController
   mealplans = MealPlan.all 
   render json: mealplans 
   end 
+
+  def create
+    mealplan = MealPlan.create(meal_plan_params)
+    render json: mealplan, status: :created 
+  end 
+
+  private
+  def meal_plan_params
+  params.permit(:title, :user_id)
+  end 
 end
--- client/src/Components/MealPlanPage.js
+++ client/src/Components/MealPlanPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import MealPlanForm from './MealPlanForm'
+import EachMealPlan from './EachMealPlan';
 
 function MealPlanPage() {
   return (
-    <div>MealPlanPage</div>
-  )
+    <>
+      <div>MealPlanPage</div>
+      <MealPlanForm />
+      <EachMealPlan/>
+    </>
+  );
 }
 
 export default MealPlanPage

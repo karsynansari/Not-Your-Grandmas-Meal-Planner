751174cb41f3a8acdda7271bf5d9ba6c618638dc
karsyn ansari <karsyn.ansari@gmail.com>
2022-04-12 10:34:37 -0600

    bug in recipe model fixed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  belongs_to :meal_plan
+  has_many :recipe_meal_plans
+  has_many :meal_plans, through: :recipe_meal_plans
   has_many :directions, dependent: :destroy 
   has_many :ingredients, dependent: :destroy
 end
